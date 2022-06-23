5c8e6b773ee14f935524431148807f2e2d95907a
andrenellin <28567813+andrenellin@users.noreply.github.com>
2022-06-23 14:43:14 -0600

    Update bs_customize_affinipay_payment_form.php

--- bs_customize_affinipay_payment_form.php
+++ bs_customize_affinipay_payment_form.php
@@ -4,14 +4,17 @@
  *  */
 
 
-add_action( 'affinipay_charge_success', 'bs_affinipay_charge_success', 10, $chargeInfo );
+/* ACTION HOOKS 
+ *
+ * */
+custom_logs('Found Me'); // Used to determine if the script is loading
+
+add_action( 'affinipay_charge_success', 'bs_affinipay_charge_success', 10, 1 );
 function bs_affinipay_charge_success(){
     custom_logs("A payment was processed using the Affinipay Wordpress Plugin");
-    custom_logs($chargeInfo);
 }
 
-add_action( 'affinipay_charge_error', 'bs_affinipay_charge_error', 10, $chargeInfo );
+add_action( 'affinipay_charge_error', 'bs_affinipay_charge_error', 10, 1 );
 function bs_affinipay_charge_error() {
     custom_logs("There was an error when processing a payment using the Affinipay Wordpress Plugin");
-    custom_logs($chargeInfo);
 }
