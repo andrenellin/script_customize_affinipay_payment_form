30d82e70e2afd07c9415c0dfe1fc2dfdc1a9b483
andrenellin <28567813+andrenellin@users.noreply.github.com>
2022-06-23 12:54:35 -0600

    Update bs_customize_affinipay_payment_form.php

--- bs_customize_affinipay_payment_form.php
+++ bs_customize_affinipay_payment_form.php
@@ -1,1 +1,17 @@
+<?php
+/* Purpose of this script:
+ * Customize the Affinipay (LawPay) Payment Pages=
+ *  */
 
+
+add_action( 'affinipay_charge_success', 'bs_affinipay_charge_success', 10, $chargeInfo );
+function bs_affinipay_charge_success(){
+    custom_logs("A payment was processed using the Affinipay Wordpress Plugin");
+    custom_logs($chargeInfo);
+}
+
+add_action( 'affinipay_charge_error', 'bs_affinipay_charge_error', 10, $chargeInfo );
+function bs_affinipay_charge_errpr() {
+    custom_logs("There was an error when processing a payment using the Affinipay Wordpress Plugin");
+    custom_logs($chargeInfo);
+}
